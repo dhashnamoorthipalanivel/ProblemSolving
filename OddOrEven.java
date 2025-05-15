b712998a7ae56113e8c2bc5001d96b127ed264ab
dhashnamoorthipalanivel <dhashnamoorthipalanivel@gmail.com>
2025-05-15 14:08:57 +0530

    1. find the number odd or Even program

---
+++ OddOrEven.java
@@ -0,0 +1,15 @@
+package ProblemSolving;
+
+// Given a positive integer n, determine whether it is odd or even. Return true if the number is even and false if the number is odd.
+
+public class OddOrEven {
+
+    static boolean isEven(int n) {
+        // code here
+        if (n % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
